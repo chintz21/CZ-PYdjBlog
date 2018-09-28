08fd23fec39e46dae30450ea3a7a5ed4a0743790
root <root@ip-172-31-88-98.ec2.internal>
2018-09-28 03:10:25 +0000

    finished views and accounts part

--- blog_app/blog_app/blog/urls.py
+++ blog_app/blog_app/blog/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('blogs/', views.BlogListView.as_view(), name="blogs"),
+    path('blog/<int:pk>', views.BlogDetailView.as_view(), name="blog-detail"),
+    path('blogger/<int:pk>', views.BlogListByAuthorView.as_view(), name='blog-by-author'),
+    path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
 ]
